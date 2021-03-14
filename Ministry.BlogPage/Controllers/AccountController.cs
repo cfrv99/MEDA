8a048ee22ffbe496faae9762551f763ea9d14367
Murad <cafarovmurad9@gmail.com>
2021-03-15 03:36:20 +0400

    Firstss

---
+++ Ministry.BlogPage/Controllers/AccountController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Ministry.BlogPage.Controllers
+{
+    public class LoginModel
+    {
+        public string UserName { get; set; }
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+    [Route("[controller]/[action]")]
+    public class AccountController : Controller
+    {
+        static private string userName = "adminmeda";
+        static private string password = "MedaAdmin21";
+        public async Task<IActionResult> Login()
+        {
+
+            return View(new LoginModel());
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            if (userName == model.UserName.ToLower() && password == model.Password)
+            {
+                List<Claim> claims = new List<Claim>();
+                claims.Add(new Claim(ClaimTypes.NameIdentifier, model.UserName));
+
+                ClaimsIdentity identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                ClaimsPrincipal principal = new ClaimsPrincipal(identity);
+                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal);
+                return Redirect("/Admin/Manager/GetNewsPage");
+            }
+            ModelState.AddModelError("Passoword", "Login yada Password sehvdir");
+            return View(model);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync();
+            return RedirectToAction("Login");
+        }
+    }
+}
